07258d50d0d90660287b8819052df103b519927b
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-11-21 23:54:17 +0700

    jwt authentication

---
+++ backend/ecommerce/src/main/java/com/pvanquochuy/ecommerce/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.pvanquochuy.ecommerce.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "alsndslandlsndlaknsdlnasldnlsadlsa ddsadqweqwewqdamlm";
+    public static final String JWT_HEADER = "Authorization";
+
+}
