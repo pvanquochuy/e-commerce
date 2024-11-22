2ea700ecce96f972c0e90c2af2a8bf5c21fc91ab
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-11-22 23:09:29 +0700

    create models

---
+++ backend/ecommerce/src/main/java/com/pvanquochuy/ecommerce/model/PaymentInformation.java
@@ -0,0 +1,21 @@
+package com.pvanquochuy.ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+
+import java.time.LocalDate;
+
+@Embeddable
+public class PaymentInformation {
+    @Column(name="cardholder_name")
+    private String cardholderName;
+    @Column(name="card_number")
+    private String cardNumber;
+    @Column(name="expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name = "cvv")
+    private String cvv;
+
+}
---
+++ backend/ecommerce/src/main/java/com/pvanquochuy/ecommerce/model/Rating.java
@@ -0,0 +1,29 @@
+package com.pvanquochuy.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "rating")
+    private double rating;
+
+    private LocalDateTime createdAt;
+
+}
