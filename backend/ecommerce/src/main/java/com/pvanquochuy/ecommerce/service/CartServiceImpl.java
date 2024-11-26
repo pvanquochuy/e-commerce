d3c4ed6466b5abe1e424036241c3c5b995a24d04
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-11-26 22:09:54 +0700

    cart service impl

--- backend/ecommerce/src/main/java/com/pvanquochuy/ecommerce/service/CartServiceImpl.java
+++ backend/ecommerce/src/main/java/com/pvanquochuy/ecommerce/service/CartServiceImpl.java
@@ -2,6 +2,8 @@ package com.pvanquochuy.ecommerce.service;
 
 import com.pvanquochuy.ecommerce.exception.ProductException;
 import com.pvanquochuy.ecommerce.model.Cart;
+import com.pvanquochuy.ecommerce.model.CartItem;
+import com.pvanquochuy.ecommerce.model.Product;
 import com.pvanquochuy.ecommerce.model.User;
 import com.pvanquochuy.ecommerce.repository.CartRepository;
 import com.pvanquochuy.ecommerce.request.AddItemRequest;
@@ -14,18 +16,60 @@ public class CartServiceImpl implements CartService {
     private CartItemService cartItemService;
     private ProductService productService;
 
+    public CartServiceImpl(CartRepository cartRepository, CartItemService cartItemService, ProductService productService) {
+        this.cartRepository = cartRepository;
+        this.cartItemService = cartItemService;
+        this.productService = productService;
+    }
+
     @Override
     public Cart createCart(User user) {
-        return null;
+        Cart cart = new Cart();
+        cart.setUser(user);
+        return cartRepository.save(cart);
     }
 
     @Override
     public String addCartItem(Long userId, AddItemRequest req) throws ProductException {
-        return "";
+        Cart cart = cartRepository.findByUserId(userId);
+        Product product = productService.findProductById(req.getProductId());
+
+        CartItem isPresent = cartItemService.isCCarttemExist(cart, product, req.getSize(), userId);
+
+        if(isPresent == null){
+            CartItem cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(req.getQuantity());
+            cartItem.setUserId(userId);
+
+            int price = req.getQuantity() * product.getDiscountedPrice();
+            cartItem.setPrice(price);
+            cartItem.setSize(req.getSize());
+
+            CartItem createdCartItem = cartItemService.createCartItem(cartItem);
+            cart.getCartItems().add(createdCartItem);
+        }
+        return "Item add to card";
     }
 
     @Override
     public Cart findUsercart(Long userId) {
-        return null;
+        Cart cart = cartRepository.findByUserId(userId);
+
+        int totalPrice = 0;
+        int totalDiscountedPrice = 0;
+        int totalItem= 0;
+
+        for(CartItem cartItem : cart.getCartItems()){
+            totalPrice = totalPrice + cartItem.getPrice();
+            totalDiscountedPrice = totalDiscountedPrice + cartItem.getDiscountedPrice();
+            totalItem = totalItem +  cartItem.getQuantity();
+        }
+        cart.setTotalDiscountedPrice(totalDiscountedPrice);
+        cart.setTotalItem(totalItem);
+        cart.setTotalPrice(totalPrice);
+        cart.setDiscounte(totalPrice-totalDiscountedPrice);
+
+        return cartRepository.save(cart);
     }
 }
